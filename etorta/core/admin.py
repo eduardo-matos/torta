48d3c5a51ff7cd16904582d7335c1ffe5a70155a
Eduardo de Matos <eduardosilva@hermes.com.br>
2013-12-04 15:51:56 -0200

    criar pagina de admin

---
+++ etorta/core/admin.py
@@ -0,0 +1,8 @@
+from core.models import Loja, Produto, Cliente, Url
+from django.contrib import admin
+
+
+admin.site.register(Loja)
+admin.site.register(Produto)
+admin.site.register(Cliente)
+admin.site.register(Url)
--- etorta/core/models.py
+++ etorta/core/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Loja(models.Model):
     nome = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.nome
+
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=100)
     loja = models.ForeignKey('Loja')
+
+    def __unicode__(self):
+        return self.nome
 
 
 class Produto(models.Model):
@@ -17,6 +23,9 @@ class Produto(models.Model):
     meu_preco = models.DecimalField(decimal_places=2, max_digits=10)
     loja = models.ForeignKey('Loja')
 
+    def __unicode__(self):
+        return self.nome
+
 
 class Url(models.Model):
     endereco = models.CharField(max_length=100)
@@ -24,3 +33,6 @@ class Url(models.Model):
     preco = models.DecimalField(decimal_places=2, max_digits=10)
     loja = models.ForeignKey('Loja')
     produto = models.ForeignKey('Produto')
+
+    def __unicode__(self):
+        return self.endereco
